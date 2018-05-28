313d91c85cdf61d26672525c67f40f7fc6414cb6
Chehir Dhawedi <thevexx@users.noreply.github.com>
2018-05-28 11:36:18 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title: string = 'My first AGM project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  markers = [
+    {
+      lat: 51.673858,
+      lng: 7.815982,
+      label: 'A',
+      draggable: true
+    },
+    {
+      lat: 51.373858,
+      lng: 7.215982,
+      label: 'B',
+      draggable: false
+    },
+    {
+      lat: 51.723858,
+      lng: 7.895982,
+      label: 'C',
+      draggable: true
+    }
+  ];
+
+ngOnInit() {
+
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAnuFywtDRLi8m3NW4p0zdX5mVmpY4-MHE'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
